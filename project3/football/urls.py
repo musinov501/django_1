bc6360b2f485134b8155d155ec514fca3e342b86
Muhammadyor <musinovmuhammaader@gmail.com>
2025-08-17 16:04:34 +0500

    Gallery is created

--- project3/football/urls.py
+++ project3/football/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home, info
+from .views import home, info, gallery
 
 urlpatterns = [
     path('', home, name = "home"),
-    path('info/', info, name = "info")
+    path('info/', info, name = "info"),
+    path('gallery/', gallery, name = "gallery")
 ]
--- project3/football/views.py
+++ project3/football/views.py
@@ -18,3 +18,10 @@ def home(request: HttpRequest):
 def info(request: HttpRequest):
     return render(request, "info.html", {"content": "Fotball App"})
 
+
+def gallery(request: HttpRequest):
+    return HttpResponse("""
+        <h1>🏟️ Football Gallery</h1>
+        <p>Look at this iconic football moment:</p>
+        <img src="https://th.bing.com/th/id/R.603dfbb635a2fcad57389881fac7e30d?rik=6pguDLycThaxaQ&pid=ImgRaw&r=0" alt="Football Image" width="400">
+    """)
